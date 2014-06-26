6ce79c4a6f832a829fb2546e03e8066effa1098d
Josh Darling <squiddarling@yahoo.com>
2014-06-26 06:29:05 -0400

    Create boots_feet.php

---
+++ boots_feet.php
@@ -0,0 +1,12 @@
+<?php
+
+$boot_foot = <<< EOD
+<!-- javascript -->
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
+EOD;
+
+echo "$boot_foot";
+?>
